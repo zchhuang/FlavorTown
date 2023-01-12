916dff7cde76a76a25b9dffa668393bada423f2f
zchhuang <zach9040@berkeley.edu>
2023-01-12 15:38:43 -0800

    commented movement var

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
 
+    // Movement Variables
     public float moveSpeed;
     private Vector2 _moveInput;
 
