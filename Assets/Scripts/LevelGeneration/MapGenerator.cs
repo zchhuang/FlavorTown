144f7aca188d2d66e4973510f1fd16183ff9674d
zchhuang <zach9040@berkeley.edu>
2023-02-12 02:48:09 -0800

    some refactors

--- Assets/Scripts/LevelGeneration/MapGenerator.cs
+++ Assets/Scripts/LevelGeneration/MapGenerator.cs
@@ -9,7 +9,7 @@ public class MapGenerator : MonoBehaviour
     Room[,] map;
 
     List<Vector2> usedPositions = new List<Vector2>();
-    int gridSizeX, gridSizeY, fullGridSizeX, fullGridSizeY, numberOfRooms = 20;
+    int gridSizeX, gridSizeY, fullGridSizeX, fullGridSizeY, numberOfRooms = 12;
 
     public GameObject layoutRoom;
     public Color startColor, endColor;
@@ -17,18 +17,16 @@ public class MapGenerator : MonoBehaviour
 
     public Transform generatorPoint;
 
-    public enum Direction { up, right, down, left }
-
     public float xOffset = 18f, yOffset = 10f;
 
     public LayerMask layerMask;
 
-    private GameObject _startRoom;
-    private GameObject _endRoom;
-    private List<GameObject> _layoutRoomObjects = new List<GameObject>();
+    // private GameObject _startRoom;
+    // private GameObject _endRoom;
+    // private List<GameObject> _layoutRoomObjects = new List<GameObject>();
 
-    public RoomPreFabs rooms;
-    private List<GameObject> _generatedOutlines = new List<GameObject>();
+    // public RoomPreFabs rooms;
+    // private List<GameObject> _generatedOutlines = new List<GameObject>();
 
     // Variables for room generation
     // public RoomCenter centerStart, centerEnd;
@@ -56,41 +54,37 @@ public class MapGenerator : MonoBehaviour
     // CreateRoomMap handles the overall room generation within the designated grid
     void CreateRoomMap()
     {
-        map = new Room[fullGridSizeX, fullGridSizeY];
+        map = new Room[fullGridSizeX+1, fullGridSizeY+1];
 
         // Instantiate start room
         map[gridSizeX, gridSizeY] = new Room(Vector2.zero, 1); // Grid positions need to be halved as they are full extents
         usedPositions.Insert(0, Vector2.zero);
         Vector2 currentPos = Vector2.zero;
 
-        //magic numbers
+        // These numbers I pulled from a github.  They're used to force branching later on in the map.
         float randomCompare = 0.2f, randomCompareStart = 0.2f, randomCompareEnd = 0.01f;
-
         for (int i = 0; i < numberOfRooms; i++)
         {
             float randomPerc = ((float)i) / (((float)numberOfRooms - 1));
             randomCompare = Mathf.Lerp(randomCompareStart, randomCompareEnd, randomPerc);
 
             currentPos = NewPosition();
-
+            // Within this loop, if the new position selected is valid, it will try to select a branching room.
             if (NumberOfNeighbors(currentPos) > 1 && Random.value > randomCompare)
             {
                 int iterations = 0;
-                do
+                while (NumberOfNeighbors(currentPos) > 1 && iterations < 20)
                 {
                     currentPos = SelectiveNewPosition();
                     iterations++;
-                } while (NumberOfNeighbors(currentPos) > 1 && iterations < 20);
+                }
                 if (iterations >= 20)
                     print("error: could not create with fewer neighbors than : " + NumberOfNeighbors(currentPos));
             }
+    
 
             // Update the Map with the newly generated room
-            print("X COORDINATE");
-            print((int)currentPos.x + gridSizeX);
-            print("Y COORDINATE");
-            print((int)currentPos.y + gridSizeY);
-            //map[(int)currentPos.x + gridSizeX, (int)currentPos.y + gridSizeY] = new Room(currentPos, 0);
+            map[(int)currentPos.x + gridSizeX, (int)currentPos.y + gridSizeY] = new Room(currentPos, 0);
             usedPositions.Insert(0, currentPos);
         }
     }
@@ -113,29 +107,36 @@ public class MapGenerator : MonoBehaviour
     bool CheckValidPos(Vector2 checkPos)
     {
         int posX = (int) checkPos.x, posY = (int) checkPos.y;
-        return (!usedPositions.Contains(checkPos) && posX < gridSizeX && posX >= -gridSizeX && posY < gridSizeY && posY >= gridSizeY); 
+        return (!usedPositions.Contains(checkPos) && posX < gridSizeX && posX >= -gridSizeX && posY < gridSizeY && posY >= -gridSizeY); 
+    }
+
+    Vector2 GetRandomAdjacentPosition(Vector2 position) 
+    {
+        Vector2[] directions = new Vector2[4] { new Vector2(1, 0), new Vector2(-1, 0), new Vector2(0, 1), new Vector2(0, -1) };
+        int posX = 0, posY = 0, dirX = 0, dirY = 0;
+        posX = (int) position.x; //capture its x, y position
+        posY = (int) position.y;
+
+        // Randomly Choose a direction to go in between (up, right, left, down)
+        int dirIndex = Random.Range(0, 4);
+        dirX = (int) directions[dirIndex].x;
+        dirY = (int) directions[dirIndex].y;
+        Vector2 newPosition = new Vector2(dirX + posX, dirY + posY);
+        return newPosition;
     }
 
     // Finds a completely random new position to build a room at 
     Vector2 NewPosition()
     {
-        int posX = 0, posY = 0, dirX = 0, dirY = 0;
         Vector2 checkPos = Vector2.zero;
-        Vector2[] directions = new Vector2[4] { new Vector2(1, 0), new Vector2(-1, 0), new Vector2(0, 1), new Vector2(0, -1) };
-
+        int iterations = 0;
         do
         {
             int index = Mathf.RoundToInt(Random.value * (usedPositions.Count - 1)); // pick a random room
-            posX = (int) usedPositions[index].x; //capture its x, y position
-            posY = (int) usedPositions[index].y;
+            checkPos = GetRandomAdjacentPosition(usedPositions[index]);
+            iterations++;
 
-            // Randomly Choose a direction to go in between (up, right, left, down)
-            int dirIndex = Random.Range(0, 4);
-            dirX = (int) directions[dirIndex].x;
-            dirY = (int) directions[dirIndex].y;
-            checkPos = new Vector2(dirX + posX, dirY + posY);
-
-        } while (!CheckValidPos(checkPos)); 
+        } while (!CheckValidPos(checkPos) && iterations < 20); 
 
         return checkPos;
     }
@@ -144,8 +145,8 @@ public class MapGenerator : MonoBehaviour
     Vector2 SelectiveNewPosition()
     {
         int index = 0, inc = 0;
-        int x = 0, y = 0;
-        Vector2 checkingPos = Vector2.zero;
+        Vector2 checkPos = Vector2.zero;
+        int iterations = 0;
         do
         {
             inc = 0;
@@ -156,39 +157,10 @@ public class MapGenerator : MonoBehaviour
                 index = Mathf.RoundToInt(Random.value * (usedPositions.Count - 1));
                 inc++;
             } while (NumberOfNeighbors(usedPositions[index]) > 1 && inc < 100);
-            x = (int)usedPositions[index].x;
-            y = (int)usedPositions[index].y;
-            bool UpDown = (Random.value < 0.5f);
-            bool positive = (Random.value < 0.5f);
-            if (UpDown)
-            {
-                if (positive)
-                {
-                    y += 1;
-                }
-                else
-                {
-                    y -= 1;
-                }
-            }
-            else
-            {
-                if (positive)
-                {
-                    x += 1;
-                }
-                else
-                {
-                    x -= 1;
-                }
-            }
-            checkingPos = new Vector2(x, y);
-        } while (usedPositions.Contains(checkingPos) || x >= gridSizeX || x < -gridSizeX || y >= gridSizeY || y < -gridSizeY);
-        if (inc >= 100)
-        { // break loop if it takes too long: this loop isnt garuanteed to find solution, which is fine for this
-            print("Error: could not find position with only one neighbor");
-        }
-        return checkingPos;
+            checkPos = GetRandomAdjacentPosition(usedPositions[index]);
+            iterations++;
+        } while (!CheckValidPos(checkPos) && iterations < 50);
+        return checkPos;
     }
 
     // NumberOfNeighbors gives us the number of neighboring rooms. This is mostly used during room generation.
@@ -226,27 +198,6 @@ public class MapGenerator : MonoBehaviour
 #endif
     }
 
-    public void MoveGenerationPoint(Direction selectedDirection)
-    {
-        switch (selectedDirection)
-        {
-            case Direction.up:
-                generatorPoint.position += new Vector3(0f, yOffset, 0f);
-                break;
-
-            case Direction.down:
-                generatorPoint.position += new Vector3(0f, -yOffset, 0f);
-                break;
-
-            case Direction.right:
-                generatorPoint.position += new Vector3(xOffset, 0f, 0f);
-                break;
-
-            case Direction.left:
-                generatorPoint.position += new Vector3(-xOffset, 0f, 0f);
-                break;
-        }
-    }
 }
 
 [System.Serializable]
